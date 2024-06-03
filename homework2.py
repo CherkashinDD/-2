537033a1af7990c3c2a6600162e671bf1ef779af
CherkashinDD <danl-0284@mail.ru>
2024-06-03 20:35:57 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_hw=12
+number_of_hours_spent=1.5
+course_name="Python"
+time_for_one_task_exercise=number_of_hours_spent/completed_hw
+print("Курс:",course_name,", всего задач:",completed_hw,", затрачено часов:",number_of_hours_spent,", среднее время выполнения",time_for_one_task_exercise,"часа")
